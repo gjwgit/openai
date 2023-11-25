566d12f6e296da4bebd13e1399b7eb49f99bfdba
Graham Williams <graham.williams@togaware.com>
2023-11-25 15:45:10 +1100

    Initial implementation of transcribe.

--- transcribe.py
+++ transcribe.py
@@ -1,1 +1,70 @@
-print("The transcribe function will be implemented shortly.")
+# -*- coding: utf-8 -*-
+#
+# MLHub toolket for OpenAI - Transcribe
+#
+# Time-stamp: <Saturday 2023-11-25 15:28:11 +1100 Graham Williams>
+#
+# Author: Graham.Williams@togaware.com
+# Licensed under GPLv3.
+# Copyright (c) Togaware Pty Ltd. All rights reserved.
+#
+# ml transcribe openai
+
+# ----------------------------------------------------------------------
+# Setup
+# ----------------------------------------------------------------------
+
+import os
+import sys
+import click
+import whisper
+
+from mlhub.pkg import get_cmd_cwd
+
+# -----------------------------------------------------------------------
+# Command line argument and options
+# -----------------------------------------------------------------------
+
+@click.command()
+@click.argument("filename",
+                default=None,
+                required=False,
+                type=click.STRING)
+@click.option("-l", "--lang",
+              default=None,
+              type=click.STRING,
+              help="The language of the source audio.")
+
+def cli(filename, lang):
+    """Transcribe audio from a file.
+
+Tested with wav, mp4.
+
+The audio is processed locally using a downlaoded OpenAI model The
+result is returned as text.
+
+    """
+
+    # -----------------------------------------------------------------------
+    # Load the required model. Just small for now.
+    # -----------------------------------------------------------------------
+
+    model = whisper.load_model("small")
+
+    # -----------------------------------------------------------------------
+    # Transcribe file or from microphone.
+    # -----------------------------------------------------------------------
+
+    if not filename:
+        sys.exit(f"openai transcribe: A filename is required.")
+        
+    path = os.path.join(get_cmd_cwd(), filename)
+
+    if not os.path.exists(path):
+        sys.exit(f"openai transcribe: File not found: {path}")
+        
+    result = model.transcribe("harvard.wav", fp16=False) # fp16 not supported on CPU
+    print(result["text"])
+
+if __name__ == "__main__":
+    cli(prog_name="transcribe")
