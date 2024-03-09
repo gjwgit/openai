f4e8d39229e01ae398b8abd6240db8aab9e30f21
Ting Tang <u7228238@anu.edu.au>
2024-03-09 11:16:16 +1100

    Change the dictionary's position to fix the bug. Reorder files and commands in yaml.

--- identify.py
+++ identify.py
@@ -22,53 +22,6 @@ import whisper
 from mlhub.pkg import get_cmd_cwd
 
 # -----------------------------------------------------------------------
-# Command line argument and options
-# -----------------------------------------------------------------------
-
-@click.command()
-@click.argument("filename",
-                default=None,
-                required=False,
-                type=click.STRING)
-
-def cli(filename):
-    """Identify the language of a media file.
-
-The audio is processed locally using a downloaded OpenAI model. The
-result is returned as text.
-
-    """
-
-    pkg = "openai"
-    cmd = "identify"
-
-    # -----------------------------------------------------------------------
-    # Load the required model. Just small for now.
-    # -----------------------------------------------------------------------
-
-    model = whisper.load_model("small")
-
-    # -----------------------------------------------------------------------
-    # Identify the language of the file.
-    # -----------------------------------------------------------------------
-
-    if not filename:
-        sys.exit(f"{pkg} {cmd}: A filename is required.")
-        
-    path = os.path.join(get_cmd_cwd(), filename)
-
-    if not os.path.exists(path):
-        sys.exit(f"{pkg} {cmd}: File not found: {path}")
-        
-    result = model.transcribe(path, fp16=False)
-    language_code = result["language"].strip()
-    language = language_code + "," + LANGUAGES[language_code].title()
-    print(language)
-    
-if __name__ == "__main__":
-    cli(prog_name="identify")
-
-# -----------------------------------------------------------------------
 # A dictionary of language codes and their names.
 # Copied from https://github.com/openai/whisper/blob/main/whisper/tokenizer.py
 # -----------------------------------------------------------------------
@@ -175,3 +128,50 @@ LANGUAGES = {
     "su": "sundanese",
     "yue": "cantonese",
 }
+
+# -----------------------------------------------------------------------
+# Command line argument and options
+# -----------------------------------------------------------------------
+
+@click.command()
+@click.argument("filename",
+                default=None,
+                required=False,
+                type=click.STRING)
+
+def cli(filename):
+    """Identify the language of a media file.
+
+The audio is processed locally using a downloaded OpenAI model. The
+result is returned as text.
+
+    """
+
+    pkg = "openai"
+    cmd = "identify"
+
+    # -----------------------------------------------------------------------
+    # Load the required model. Just small for now.
+    # -----------------------------------------------------------------------
+
+    model = whisper.load_model("small")
+
+    # -----------------------------------------------------------------------
+    # Identify the language of the file.
+    # -----------------------------------------------------------------------
+
+    if not filename:
+        sys.exit(f"{pkg} {cmd}: A filename is required.")
+        
+    path = os.path.join(get_cmd_cwd(), filename)
+
+    if not os.path.exists(path):
+        sys.exit(f"{pkg} {cmd}: File not found: {path}")
+        
+    result = model.transcribe(path, fp16=False)
+    language_code = result["language"].strip()
+    language = language_code + "," + LANGUAGES[language_code].title()
+    print(language)
+    
+if __name__ == "__main__":
+    cli(prog_name="identify")
